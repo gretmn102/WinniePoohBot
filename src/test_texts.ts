c54657028302c0be76842abd6992274b801c7e33
Aleksandr Yakovlev <keloero@oreolek.me>
2025-11-15 13:07:07 +0600

    Тест текстов

--- src/test_texts.ts
+++ src/test_texts.ts
@@ -1,10 +1,11 @@
 import fs from "node:fs"
 
-const db = fs.readFileSync("birthday_db.json")
+let db = fs.readFileSync("birthday_db.json").toString()
 if (typeof db === "undefined" || db === null || db.length === 0) {
   console.error("no db")
   process.exit()
 }
+db = JSON.parse(db)
 db.forEach(function(birthday: any) {
-  console.log(birthday.congratulations + "\n")
+  console.log(birthday.congratulations)
 })
