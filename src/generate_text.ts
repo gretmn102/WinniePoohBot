65ebd1b741f0e7a0a333ea05248ff200d7767596
Aleksandr Yakovlev <keloero@oreolek.me>
2025-11-16 00:16:01 +0600

    Падежи

--- src/generate_text.ts
+++ src/generate_text.ts
@@ -5,9 +5,9 @@ export default function(birthday: any): string {
     if (typeof birthday.nicks !== "undefined" && birthday.nicks.length > 0) {
       const amount = birthday.nicks.search(",")
       if (amount > 1) {
-        return "Сегодня день рождения отмечают: " + birthday.nicks + "."
+        return "Сегодня день рождения отмечает: " + birthday.nicks + "."
       } else {
-        return "Сегодня день рождения у " + birthday.nicks + "."
+        return "Сегодня день рождения отмечают:" + birthday.nicks + "."
       }
     }
   }
